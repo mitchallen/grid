a9fb5430b02ddfb59ae5f5afe86f03392d03e9ac
Mitch Allen <mitch.c.allen@gmail.com>
2016-09-30 13:30:29 -0700

    added mult-grid test suite

---
+++ test/multi-test.js
@@ -0,0 +1,62 @@
+/**
+    Module: @mitchallen/grid
+      Test: multi-test
+    Author: Mitch Allen
+*/
+
+"use strict";
+
+var request = require('supertest'),
+    should = require('should'),
+    modulePath = "../index";
+
+describe('module multi grid test', function() {
+
+    var _module = null;
+
+    before(function(done) {
+        // Call before all tests
+        delete require.cache[require.resolve(modulePath)];
+        _module = require(modulePath);
+        done();
+    });
+
+    after(function(done) {
+        // Call after all tests
+        done();
+    });
+
+    beforeEach(function(done) {
+        // Call before each test
+        done();
+    });
+
+    afterEach(function(done) {
+        // Call after eeach test
+        done();
+    });
+
+    it('get with x and y parameters set to size minus should return value', function(done) {
+        let sizeX = 20;
+        let sizeY = 10;
+        var grid0 = _module.create({ x: sizeX, y: sizeY });
+        should.exist(grid0);
+        var grid1 = _module.create({ x: sizeX, y: sizeY });
+        should.exist(grid1);
+        let tX = sizeX-1;
+        let tY = sizeY-1;
+        let tValues = [ 100, 200 ];
+        // Set values
+        var condition0 = grid0.set(tX,tY,tValues[0]);
+        condition0.should.eql(true);
+        var condition1 = grid1.set(tX,tY,tValues[1]);
+        condition1.should.eql(true);
+        // Get and test values
+        var result0 = grid0.get(tX,tY);
+        result0.should.eql(tValues[0]);
+        var result1 = grid1.get(tX,tY);
+        result1.should.eql(tValues[1]);
+        done();
+    });
+
+});
